cf7c2f81e454f08d84b3d477d02d0a8383408e55
Babrandon <ashworth3633@gmail.com>
2016-09-13 16:56:08 -0500

    changed echo commands to sed commands

--- main.sh
+++ main.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
 
-su -
 
-apt-get update
-apt-get install -y git python-pip vim
-apt-get upgrade -y python
-echo 127.0.0.1 localhost $HOSTNAME >> /etc/hosts
+
+sudo apt-get update
+sudo apt-get install -y git python-pip vim
+sudo apt-get upgrade -y python
+
+
+sudo touch hosts
+sudo sed -e "s/[ 	]*127.0.0.1[ 	]*localhost[ 	]*$/127.0.0.1 localhost $HOSTNAME/" > hosts
+sudo cp hosts /etc/hosts
 
 chown cc:cc /home/cc 
 cd /home/cc
@@ -20,13 +24,17 @@ git clone https://github.com/BAbrandon/ScriptsForHeat.git
 
 cp /ScriptsForHeat/local.conf .
 
-echo net.ipv4.ip_forward=1 >> /etc/sysctl.conf
-echo net.ipv4.conf.default.rp_filter=0 >> /etc/sysctl.conf
-echo net.ipv4.conf.all.rp_filter=0 >> /etc/sysctl.conf
-iptables -t nat -A POSTROUTING -o eth0 -j MASQUERADE
+touch sysctl.conf
+sudo sed -e "s/as needed.$/as needed.\n net.ipv4.ip_forward=1\n/" /etc/sysctl.conf >  sysctl.conf
+
+sudo sed -e "s/as needed.$/as needed.\n net.ipv4.conf.default.rp.filter=0\n/" sysctl.conf > sysctl.conf
+
+sudo sed -e "s/as needed.$/as needed.\n net.ipv4.conf.all.rp.filter=0\n/" sysctl.conf > sysctl.conf
+
+sudo cp sysctl.conf /etc/sysctl.conf
+
+sudo iptables -t nat -A POSTROUTING -o eth0 -j MASQUERADE
 
 ./devstack/stack.sh
 
-runcmd:
-  - su -l cc ./start.sh
 
